2d495cd00ae4e15e61ec2618e1016445c16e7f2e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-07 13:24:36 -0300

    [ADD] clv_base_jcafb (Copied from branch 14.0)

---
+++ clv_base_jcafb/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Base Module (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'Base Module Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_base',
+    ],
+    'data': [
+        # # 'views/mfile_menu_view.xml',
+        # # 'data/annotation_seq.xml',
+        # 'data/global_settings_filestore.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
