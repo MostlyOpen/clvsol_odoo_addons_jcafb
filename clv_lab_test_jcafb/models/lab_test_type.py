8b7172adbbf57d8ecd0846278c1099bc44be1913
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-09 21:32:41 -0300

    [ADD] clv_lab_test_jcafb

---
+++ clv_lab_test_jcafb/models/lab_test_type.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class LabTestType(models.Model):
+    _inherit = 'clv.lab_test.type'
+
+    survey_id = fields.Many2one(
+        comodel_name='survey.survey',
+        string='Survey Type')
+
+    template_file_name_result = fields.Char(
+        string='Template File Name (Result)',
+        required=False,
+        help="Template File Name (Result)"
+    )
+
+    template_file_name_report = fields.Char(
+        string='Template File Name (Report)',
+        required=False,
+        help="Template File Name (Report)"
+    )
---
+++ clv_lab_test_jcafb/wizard/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import lab_test_request_receive
+from . import lab_test_result_approve
+from . import lab_test_report_approve
+from . import lab_test_report_export_xls
+from . import lab_test_result_export_xls
+from . import lab_test_request_associate_to_set
+from . import lab_test_report_associate_to_set
+from . import lab_test_result_set_survey_user_input
+from . import lab_test_type_criteria_setup
+from . import lab_test_result_cirteria_updt_from_survey
+from . import lab_test_result_survey_user_input_refresh
+from . import lab_test_result_survey_user_input_validate
+from . import lab_test_result_survey_user_input_set_in_progress
