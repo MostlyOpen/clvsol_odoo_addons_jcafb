56f86185d644330c260110930ea03f3d967fcd47
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-10 21:13:48 -0300

    [ADD] clv_patient_jcafb (Copied from branch 14.0)

---
+++ clv_patient_jcafb/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Patient (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'Patient Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_base_jcafb',
+        'clv_patient',
+        'clv_document',
+        # 'clv_community',
+        'clv_event',
+        'clv_lab_test',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/patient_seq.xml',
+        'data/document.xml',
+        # 'data/community_member.xml',
+        'data/event_attendee.xml',
+        'data/lab_test.xml',
+        'data/set_element.xml',
+        'views/patient_code_view.xml',
+        'views/document_view.xml',
+        # 'views/community_member_view.xml',
+        'views/event_attendee_view.xml',
+        'views/lab_test_view.xml',
+        'wizard/patient_associate_to_set_view.xml',
+        'wizard/patient_document_setup_view.xml',
+        'wizard/patient_lab_test_request_setup_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
---
+++ clv_patient_jcafb/wizard/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import patient_associate_to_set
+from . import patient_document_setup
+from . import patient_lab_test_request_setup
