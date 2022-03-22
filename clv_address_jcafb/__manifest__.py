67b6029e524d1378f4cceb2abe58e3f7e7c807f0
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-22 20:27:51 -0300

    clv_address_jcafb (Copied from branch 14.0)

---
+++ clv_address_jcafb/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Address (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'Address Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_address',
+        'clv_document',
+        'clv_community',
+        'clv_event',
+        'clv_lab_test',
+        'clv_base_jcafb',
+    ],
+    'data': [
+        # 'data/address_seq.xml',
+        'data/document.xml',
+        'data/community_member.xml',
+        'data/event_attendee.xml',
+        'data/lab_test.xml',
+        'security/ir.model.access.csv',
+        'views/address_code_view.xml',
+        'views/document_view.xml',
+        'views/community_member_view.xml',
+        'views/event_attendee_view.xml',
+        'views/lab_test_view.xml',
+        'wizard/address_document_setup_view.xml',
+        'wizard/address_lab_test_request_setup_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
