832f589014e4db2319897bddbd4c157d531711e5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-24 17:50:23 -0300

    [ADD] clv_address_aux_jcafb (Copied from branch 14.0)

---
+++ clv_address_aux_jcafb/models/address_aux_code.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AddressAux(models.Model):
+    _name = "clv.address_aux"
+    _inherit = 'clv.address_aux', 'clv.abstract.code'
+
+    code = fields.Char(string='Address Code', required=False, default=False)
+    code_sequence = fields.Char(default='clv.address.code')
+
+    def _address_aux_set_code(self):
+
+        for address_aux in self:
+
+            if address_aux.code is False:
+
+                vals = {}
+
+                if address_aux.related_address_id.id is not False:
+                    vals['code'] = address_aux.related_address_id.code
+
+                else:
+                    vals['code'] = '/'
+
+                address_aux.write(vals)
