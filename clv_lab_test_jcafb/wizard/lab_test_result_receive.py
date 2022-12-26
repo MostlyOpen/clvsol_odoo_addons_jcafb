2dff9fe3286c1c26e79f50ccf35beee651ea9272
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-12-26 17:05:12 -0300

    [IMP] clv_lab_test_jcafb (IMP wizard/lab_test_result_receive)

--- clv_lab_test_jcafb/wizard/lab_test_result_receive.py
+++ clv_lab_test_jcafb/wizard/lab_test_result_receive.py
@@ -45,168 +45,24 @@ class LabTestResultReceive(models.TransientModel):
         default=lambda *a: datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     )
 
-    def _default_dir_path_result(self):
-        file_store_path = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_filestore_path', '').strip()
-        lab_test_result_files_directory_xls = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_lab_test_result_files_directory_xls', '').strip()
-        return file_store_path + '/' + lab_test_result_files_directory_xls
-    dir_path_result = fields.Char(
-        string='Directory Path (Result)',
-        required=True,
-        help="Directory Path (Result)",
-        default=_default_dir_path_result
-    )
-
-    def _default_file_name_result(self):
-        lab_test_result_file_name_xls = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_lab_test_result_file_name_xls', '').strip()
-        return lab_test_result_file_name_xls
-    file_name_result = fields.Char(
-        string='File Name (Result)',
-        required=True,
-        help="File Name (Result)",
-        default=_default_file_name_result
-    )
-
-    use_template_result = fields.Boolean(string='Use Template (Result)', default=True)
-
-    def _default_templates_dir_path_result(self):
-        file_store_path = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_filestore_path', '').strip()
-        lab_test_result_files_directory_templates = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_lab_test_result_files_directory_templates', '').strip()
-        return file_store_path + '/' + lab_test_result_files_directory_templates
-    templates_dir_path_result = fields.Char(
-        string='Template Directory Path (Result)',
-        required=True,
-        help="Template Directory Path (Result)",
-        default=_default_templates_dir_path_result
-    )
-
-    def _default_dir_path_report(self):
-        file_store_path = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_filestore_path', '').strip()
-        lab_test_report_files_directory_xls = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_lab_test_report_files_directory_xls', '').strip()
-        return file_store_path + '/' + lab_test_report_files_directory_xls
-    dir_path_report = fields.Char(
-        string='Directory Path (Report)',
-        required=True,
-        help="Directory Path (Report)",
-        default=_default_dir_path_report
-    )
-
-    def _default_file_name_report(self):
-        lab_test_report_file_name_xls = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_lab_test_report_file_name_xls', '').strip()
-        return lab_test_report_file_name_xls
-    file_name_report = fields.Char(
-        string='File Name (Report)',
-        required=True,
-        help="File Name (Report)",
-        default=_default_file_name_report
-    )
-
-    use_template_report = fields.Boolean(string='Use Template (Report)', default=True)
-
-    def _default_templates_dir_path_report(self):
-        file_store_path = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_filestore_path', '').strip()
-        lab_test_report_files_directory_templates = self.env['ir.config_parameter'].sudo().get_param(
-            'clv.global_settings.current_lab_test_report_files_directory_templates', '').strip()
-        return file_store_path + '/' + lab_test_report_files_directory_templates
-    templates_dir_path_report = fields.Char(
-        string='Template Directory Path (Report)',
-        required=True,
-        help="Template Directory Path (Report)",
-        default=_default_templates_dir_path_report
-    )
-
     def do_lab_test_result_receive(self):
         self.ensure_one()
-
-        LabTestResult = self.env['clv.lab_test.result']
-        LabTestReport = self.env['clv.lab_test.report']
 
         current_phase_id = int(self.env['ir.config_parameter'].sudo().get_param(
             'clv.global_settings.current_phase_id', '').strip())
 
         for lab_test_result in self.lab_test_result_ids:
 
-            _logger.info(u'%s %s %s', '>>>>>', lab_test_result.code, lab_test_result.ref_name)
-
-            ref_id = lab_test_result.ref_id._name + ',' + str(lab_test_result.ref_id.id)
+            _logger.info(u'%s %s %s', '>>>>>', lab_test_result.phase_id.id, lab_test_result.ref_name)
 
             if (lab_test_result.phase_id.id == current_phase_id) and \
-               (lab_test_result.state == 'draft'):
+               (lab_test_result.state == 'new'):
 
                 _logger.info(u'%s %s %s', '>>>>>', self.employee_id.name, self.date_received)
 
-                lab_test_result.employee_id = self.employee_id
+                lab_test_result.employee_id_request = self.employee_id
                 lab_test_result.date_received = self.date_received
                 lab_test_result.state = 'received'
-
-                # if lab_test_result.state not in ['draft', 'cancelled']:
-
-                for lab_test_type in lab_test_result.lab_test_type_ids:
-
-                    _logger.info(u'%s %s', '>>>>>>>>>>', lab_test_type.name)
-
-                    # criteria = []
-                    # for criterion in lab_test_type.criterion_ids:
-                    #     if criterion.result_display:
-                    #         criteria.append((0, 0, {'code': criterion.code,
-                    #                                 'name': criterion.name,
-                    #                                 'sequence': criterion.sequence,
-                    #                                 'normal_range': criterion.normal_range,
-                    #                                 'unit_id': criterion.unit_id.id,
-                    #                                 }))
-
-                    values = {
-                        'code_sequence': 'clv.lab_test.result.code',
-                        'lab_test_type_id': lab_test_type.id,
-                        'survey_id': lab_test_type.survey_id.id,
-                        'ref_id': ref_id,
-                        'lab_test_result_id': lab_test_result.id,
-                        'phase_id': lab_test_result.phase_id.id,
-                        # 'criterion_ids': criteria,
-                    }
-                    lab_test_result = LabTestResult.create(values)
-
-                    # lab_test_result.lab_test_result_export_xls(self.dir_path_result, self.file_name_result,
-                    #                                            self.use_template_result,
-                    #                                            self.templates_dir_path_result)
-
-                    _logger.info(u'%s %s', '>>>>>>>>>>>>>>>', lab_test_result.code)
-
-                    # criteria = []
-                    # for criterion in lab_test_type.criterion_ids:
-                    #     if criterion.report_display:
-                    #         criteria.append((0, 0, {'code': criterion.code,
-                    #                                 'name': criterion.name,
-                    #                                 'sequence': criterion.sequence,
-                    #                                 'normal_range': criterion.normal_range,
-                    #                                 'unit_id': criterion.unit_id.id,
-                    #                                 }))
-
-                    values = {
-                        'code_sequence': 'clv.lab_test.report.code',
-                        'lab_test_type_id': lab_test_type.id,
-                        'ref_id': ref_id,
-                        'lab_test_result_id': lab_test_result.id,
-                        'lab_test_result_id': lab_test_result.id,
-                        'phase_id': lab_test_result.phase_id.id,
-                        # 'criterion_ids': criteria,
-                    }
-                    lab_test_report = LabTestReport.create(values)
-
-                    # lab_test_report.lab_test_report_export_xls(self.dir_path_report, self.file_name_report,
-                    #                                            self.use_template_report,
-                    #                                            self.templates_dir_path_report)
-
-                    _logger.info(u'%s %s', '>>>>>>>>>>>>>>>', lab_test_report.code)
-
-                    # lab_test_result.lab_test_report_id = lab_test_report.id
+                lab_test_result.reg_state = 'revised'
 
         return True
