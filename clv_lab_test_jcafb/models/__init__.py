4dddbf6d8f193a988c00f66ad5b3068691f1436e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2023-01-12 11:59:29 -0300

    [REF] clv_lab_test_jcafb (REM lab_test_request)

--- clv_lab_test_jcafb/models/__init__.py
+++ clv_lab_test_jcafb/models/__init__.py
@@ -3,8 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import lab_test_type
-from . import lab_test_request
-from . import lab_test_request_code
+# from . import lab_test_request
+# from . import lab_test_request_code
 from . import lab_test_result
 from . import lab_test_result_code
 from . import lab_test_result_export_xls
