a99a5bea04e5331b4bc26c42977180efb7368e07
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2023-01-13 18:02:00 -0300

    [REF] clv_residence_jcafb

--- clv_residence_jcafb/models/lab_test.py
+++ clv_residence_jcafb/models/lab_test.py
@@ -8,11 +8,6 @@ from odoo import fields, models
 class Residence(models.Model):
     _inherit = 'clv.residence'
 
-    # lab_test_request_ids = fields.One2many(
-    #     string='Lab Test Requests',
-    #     comodel_name='clv.lab_test.request',
-    #     compute='_compute_lab_test_request_ids_and_count',
-    # )
     lab_test_request_ids = fields.Integer(
         string='Lab Test Requests',
         compute='_compute_lab_test_request_ids_and_count',
@@ -26,27 +21,6 @@ class Residence(models.Model):
         compute='_compute_lab_test_request_ids_and_count',
     )
 
-    # def _compute_lab_test_request_ids_and_count(self):
-    #     for record in self:
-
-    #         # search_domain = [
-    #         #     ('ref_id', '=', self._name + ',' + str(record.id)),
-    #         # ]
-    #         search_domain = [
-    #             ('ref_name', '=', record.name),
-    #             ('ref_code', '=', record.code),
-    #         ]
-    #         lab_test_requests_2 = self.env['clv.lab_test.request'].search(search_domain)
-
-    #         if record.phase_id.id is not False:
-    #             search_domain.append(
-    #                 ('phase_id.id', '=', record.phase_id.id),
-    #             )
-    #         lab_test_requests = self.env['clv.lab_test.request'].search(search_domain)
-
-    #         record.count_lab_test_requests = len(lab_test_requests)
-    #         record.count_lab_test_requests_2 = len(lab_test_requests_2)
-    #         record.lab_test_request_ids = [(6, 0, lab_test_requests.ids)]
     def _compute_lab_test_request_ids_and_count(self):
         for record in self:
 
@@ -71,9 +45,6 @@ class Residence(models.Model):
     def _compute_lab_test_result_ids_and_count(self):
         for record in self:
 
-            # search_domain = [
-            #     ('ref_id', '=', self._name + ',' + str(record.id)),
-            # ]
             search_domain = [
                 ('ref_name', '=', record.name),
                 ('ref_code', '=', record.code),
@@ -90,11 +61,6 @@ class Residence(models.Model):
             record.count_lab_test_results_2 = len(lab_test_results_2)
             record.lab_test_result_ids = [(6, 0, lab_test_results.ids)]
 
-    # lab_test_report_ids = fields.One2many(
-    #     string='Lab Test Reports',
-    #     comodel_name='clv.lab_test.report',
-    #     compute='_compute_lab_test_report_ids_and_count',
-    # )
     lab_test_report_ids = fields.Integer(
         string='Lab Test Reports',
         compute='_compute_lab_test_report_ids_and_count',
@@ -108,27 +74,6 @@ class Residence(models.Model):
         compute='_compute_lab_test_report_ids_and_count',
     )
 
-    # def _compute_lab_test_report_ids_and_count(self):
-    #     for record in self:
-
-    #         # search_domain = [
-    #         #     ('ref_id', '=', self._name + ',' + str(record.id)),
-    #         # ]
-    #         search_domain = [
-    #             ('ref_name', '=', record.name),
-    #             ('ref_code', '=', record.code),
-    #         ]
-    #         lab_test_reports_2 = self.env['clv.lab_test.report'].search(search_domain)
-
-    #         if record.phase_id.id is not False:
-    #             search_domain.append(
-    #                 ('phase_id.id', '=', record.phase_id.id),
-    #             )
-    #         lab_test_reports = self.env['clv.lab_test.report'].search(search_domain)
-
-    #         record.count_lab_test_reports = len(lab_test_reports)
-    #         record.count_lab_test_reports_2 = len(lab_test_reports_2)
-    #         record.lab_test_report_ids = [(6, 0, lab_test_reports.ids)]
     def _compute_lab_test_report_ids_and_count(self):
         for record in self:
 
