e08c6e253b7164ffe957c3fe09d5ea855b4ad9d7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2023-02-24 15:54:44 -0300

    [IMP] clv_residence_jcafb (IMP models/document_code.py)

--- clv_residence_jcafb/models/residence_code.py
+++ clv_residence_jcafb/models/residence_code.py
@@ -2,7 +2,8 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class Residence(models.Model):
@@ -31,3 +32,20 @@ class Residence(models.Model):
                 vals['code'] = '/'
 
                 residence.write(vals)
+
+    @api.constrains('code')
+    def _check_code(self):
+
+        for record in self:
+
+            if record.code is not False:
+
+                sequence_str = ''
+                for c in record.code:
+                    if c.isdigit():
+                        sequence_str = sequence_str + c
+                sequence_str = sequence_str[:len(sequence_str) - 2]
+                format_code = self.env['clv.abstract.code'].format_code(sequence_str)
+
+                if record.code != format_code:
+                    raise UserError(u'Invalid Code!')
