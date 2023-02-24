7befdfb2df69ac2e82d0052e6723de2d0cdca40f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2023-02-24 15:54:15 -0300

    [IMP] clv_patient_rec_jcafb (IMP models/document_code.py)

--- clv_patient_rec_jcafb/models/patient_rec_code.py
+++ clv_patient_rec_jcafb/models/patient_rec_code.py
@@ -2,7 +2,8 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class PatientRec(models.Model):
@@ -11,3 +12,20 @@ class PatientRec(models.Model):
 
     code = fields.Char(string='Patient Code', required=False, default=False)
     code_sequence = fields.Char(default='clv.person.code')
+
+    @api.constrains('code')
+    def _check_code(self):
+
+        for record in self:
+
+            if record.code is not False:
+
+                sequence_str = ''
+                for c in record.code:
+                    if c.isdigit():
+                        sequence_str = sequence_str + c
+                sequence_str = sequence_str[:len(sequence_str) - 2]
+                format_code = self.env['clv.abstract.code'].format_code(sequence_str)
+
+                if record.code != format_code:
+                    raise UserError(u'Invalid Code!')
